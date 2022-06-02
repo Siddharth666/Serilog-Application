5d988326a017b6b2cf20c4850f2d792d150edfa9
Siddharth Singha <siddharth.singha@omaticsoftware.com>
2022-06-02 13:08:51 +0530

    Add project files.

---
+++ Application/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesControllers : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<ValuesControllers> _logger;
+
+        public ValuesControllers(ILogger<ValuesControllers> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetValues")]
+        public void Hey()
+        {
+            _logger.LogWarning("User is not valid");
+        }
+    }
+}
---
+++ Application/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastControllers : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastControllers> _logger;
+
+        public WeatherForecastControllers(ILogger<WeatherForecastControllers> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+           
+            _logger.LogInformation("Logging Info");
+            _logger.LogError("An error has occured");
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
